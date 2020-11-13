80ed5113df3647ffb30eabca33418bb57119d8dd
GET Academy <terje@kolderup.net>
2020-11-13 11:12:55 +0100

    Initial commit

---
+++ AbaxSimple/Boat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbaxSimple
+{
+    class Boat : VehicleWithMaxSpeed
+    {
+        public int GrossTonnage { get; }
+
+        public Boat(string regNo, int effect, int maxSpeed, int grossTonnage) 
+            : base(maxSpeed, regNo, effect, "knop")
+        {
+            GrossTonnage = grossTonnage;
+        }
+
+        public override void Run()
+        {
+            Console.WriteLine("Kjører båten");
+        }
+
+        public override void PrintInformation()
+        {
+            Console.WriteLine($" ** Båt ** ");
+            base.PrintInformation();
+            Console.WriteLine($" - Bruttotonnasje = {GrossTonnage}kg");
+        }
+    }
+}
---
+++ AbaxSimple/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbaxSimple
+{
+    abstract class Vehicle
+    {
+        public string RegNo { get; }
+        public int Effect { get; }
+
+        protected Vehicle(string regNo, int effect)
+        {
+            RegNo = regNo;
+            Effect = effect;
+        }
+
+        public virtual void PrintInformation()
+        {
+            Console.WriteLine($" - Reg.nr. = {RegNo}");
+            Console.WriteLine($" - Effekt = {Effect}kw");
+        }
+
+        public abstract void Run();
+    }
+}
